a9b9d45c1e98761ebaf81d996141ced622b268c9
sssgordon <gso7@uwo.ca>
2019-10-30 16:53:18 +0100

    Added header into home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Plant from "../utils/Plant";
+import Header from "../navigationBar/navigationbar";
 
 export default function Home(props) {
   return (
     <div>
-      {/* nav and image */}
+      <Header />
       <main>
+        <img src="https://as1.ftcdn.net/jpg/02/65/59/40/500_F_265594015_RTi1Vitb6h3jA6R64A2Ob3TIuvdJLkmc.jpg" />
         {props.plants.map(plant => {
           return (
             <Plant
