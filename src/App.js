65871e2fb2cd30ef722fd70431a584c98e97e0ee
sssgordon <gso7@uwo.ca>
2019-10-31 10:46:09 +0100

    Created about page and established routing

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Route } from "react-router-dom";
 import SnowContainer from "./components/Snow";
 import Header from "./components/navigationBar/navigationbar";
 import DesertContainer from "./components/Desert";
+import About from "./About/index";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Route exact path="/" component={HomeContainer} />
+      <Route exact path="/about" component={About} />
       <Route exact path="/snow" component={SnowContainer} />
       <Route exact path="/desert" component={DesertContainer} />
     </div>
