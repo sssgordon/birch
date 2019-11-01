e8188a9560fb17963a46d145f50875ee8ad84205
sssgordon <gso7@uwo.ca>
2019-11-01 14:40:01 +0100

    Added styling to category pages

--- src/components/Snow/Snow.js
+++ src/components/Snow/Snow.js
@@ -1,25 +1,32 @@
 import React from "react";
 import Plant from "../utils/Plant";
+import "./snow.css";
 
 export default function Snow(props) {
   return (
-    <div>
+    <div className="snow">
       <main>
-        <img src="https://images.pexels.com/photos/954710/pexels-photo-954710.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" />
-        {props.snowPlants.map(plant => {
-          return (
-            <Plant
-              path={props.path}
-              selectPlant={props.selectPlant}
-              name={plant.name}
-              description={plant.description}
-              price={plant.price}
-              imageUrl={plant.imageUrl}
-              id={plant.id}
-              key={plant.id}
-            />
-          );
-        })}
+        <h1 className="title">SNOW</h1>
+        <img
+          className="banner"
+          src="https://images.pexels.com/photos/954710/pexels-photo-954710.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+        />
+        <div className="plants-display">
+          {props.snowPlants.map(plant => {
+            return (
+              <Plant
+                path={props.path}
+                selectPlant={props.selectPlant}
+                name={plant.name}
+                description={plant.description}
+                price={plant.price}
+                imageUrl={plant.imageUrl}
+                id={plant.id}
+                key={plant.id}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
