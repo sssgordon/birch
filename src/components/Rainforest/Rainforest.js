6632489a8c8859ebb1dced7e8c8defde52b4b309
sssgordon <gso7@uwo.ca>
2019-11-01 15:49:54 +0100

    Created css for about page

--- src/components/Rainforest/Rainforest.js
+++ src/components/Rainforest/Rainforest.js
@@ -7,10 +7,13 @@ export default function Rainforest(props) {
     <div className="rainforest">
       <main>
         <h1 className="title">RAINFOREST</h1>
-        <img
-          className="banner"
-          src="https://images.pexels.com/photos/927414/pexels-photo-927414.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-        />
+        <div className="banner-container">
+          <img
+            className="banner"
+            src="https://images.pexels.com/photos/927414/pexels-photo-927414.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          />
+        </div>
+
         <div className="plants-display">
           {props.rainforestPlants.map(plant => {
             return (
