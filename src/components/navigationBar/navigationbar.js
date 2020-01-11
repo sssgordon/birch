c490750d381fe1a3c4eacb29c09f4502ac261227
sssgordon <gso7@uwo.ca>
2020-01-11 20:16:42 +0100

    Style navigation bar

--- src/components/navigationBar/navigationbar.js
+++ src/components/navigationBar/navigationbar.js
@@ -3,27 +3,27 @@ import { Link } from "react-router-dom";
 import "./navigationbar.css";
 
 export default class Header extends React.Component {
-  render() {
-    return (
-      <div className="navbar-div">
-        <header className="section-navbar" name="navigation">
-          <Link to="/">Home </Link>
-          <Link to="/about">About </Link>
-          <Link to="/desert">Desert </Link>
-          <Link to="/snow">Snow </Link>
-          <Link to="/rainforest">Rainforest </Link>
-          <Link to="/wishlist">
-            <button className="wishlist-button">
-              <b>Wishlist</b>
-            </button>
-          </Link>
-          <Link to="/basket">
-            <button className="basket-button">
-              <b>Basket</b>
-            </button>
-          </Link>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="navigation">
+                <header className="navigation__header">
+                    <Link to="/">Home </Link>
+                    <Link to="/about">About </Link>
+                    <Link to="/desert">Desert </Link>
+                    <Link to="/snow">Snow </Link>
+                    <Link to="/rainforest">Rainforest </Link>
+                    <Link to="/wishlist">
+                        <button className="wishlist-button">
+                            <b>Wishlist</b>
+                        </button>
+                    </Link>
+                    <Link to="/basket">
+                        <button className="basket-button">
+                            <b>Basket</b>
+                        </button>
+                    </Link>
+                </header>
+            </div>
+        );
+    }
 }
