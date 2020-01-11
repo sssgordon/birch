1aeda0c05f2a40a1a799d70c061a7053c12d7129
sssgordon <gso7@uwo.ca>
2020-01-11 22:05:15 +0100

    Style Desert

--- src/components/Desert/Desert.js
+++ src/components/Desert/Desert.js
@@ -10,7 +10,7 @@ export default function Desert(props) {
             <main>
                 <Banner
                     title="DESERT"
-                    img="https://images.pexels.com/photos/80454/tree-desert-namibia-dead-vlei-80454.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+                    img="https://images.unsplash.com/photo-1488197047962-b48492212cda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1347&q=80"
                 />
                 <div className="plants-display">
                     {props.desertPlants.map(plant => {
