e19306afd90a0e4555af4ffce0c3487bcfba3658
sssgordon <gso7@uwo.ca>
2020-01-11 17:58:55 +0100

    Save

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,11 @@
 import { combineReducers } from "redux";
 import plants from "./plants";
-
-// import rainforest from "./rainforest";
 import basket from "./basket";
 import wishlist from "./wishlist";
 import products from "./products";
 
 export default combineReducers({
     plants,
-
-    // rainforest,
     basket,
     wishlist,
     products
